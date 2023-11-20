6a6b8fd0fc71494d49bd1f0f526ab097ceac5b7a
vlad030725 <udinvladl22@gmail.com>
2023-11-20 14:04:09 +0300

    Сгенерил базу на ноут

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VladPC.DAL;
 
 namespace TestProject
 {
@@ -10,6 +11,10 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
+            using (CompContext db = new CompContext())
+            {
+                db.Company.Select(i => i).ToList();
+            }
         }
     }
 }
