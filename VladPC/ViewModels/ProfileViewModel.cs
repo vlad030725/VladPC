e2edb03373fc634918fef7309fd55e64ef3bb7c9
vlad030725 <124042722+vlad030725@users.noreply.github.com>
2023-12-11 01:09:16 +0300

    Add_UserCystom

--- VladPC/ViewModels/ProfileViewModel.cs
+++ VladPC/ViewModels/ProfileViewModel.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using VladPC.BLL.Interfaces;
+using VladPC.ViewModels.Base;
 
 namespace VladPC.ViewModels
 {
-    internal class ProfileViewModel
+    internal class ProfileViewModel : ViewModel
     {
+        ICustomService _customService;
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand UserCustomsCommand { get; set; }
+
+        //public void UserCustoms(object obj) => CurrentView;
     }
 }
